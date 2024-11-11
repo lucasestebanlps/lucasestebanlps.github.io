7dfd0519521446c6acb1e0c35339e33412727933
Lucas <lucasestebanlps@gmail.com>
2024-11-11 22:15:52 +0100

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-
 export default defineConfig({
     plugins: [react()],
-    base: '/lucasestebanlps.github.io/',
+    base: '/lucasestebanlps.github.io/',  // Base del repositorio
     build: {
-        outDir: 'dist',
+        outDir: 'dist',                   // Carpeta de salida
+        rollupOptions: {
+            input: 'index.html',          // Archivo HTML principal
+        }
     },
 });
